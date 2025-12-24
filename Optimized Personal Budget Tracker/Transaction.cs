81e2127156310fee1d5b5b77cc295679c3ae6f5c
GSS1717 <gauravsorathiya33@gmail.com>
2025-12-24 17:19:51 +0530

    Transaction file added.

---
+++ Optimized Personal Budget Tracker/Transaction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optimized_Personal_Budget_Tracker
+{
+    // highlight-start
+    /// <summary>
+    /// Represents the type of a financial transaction.
+    /// </summary>
+    public enum TransactionType
+    {
+        /// <summary>
+        /// A transaction that adds funds to the budget.
+        /// </summary>
+        Income,
+
+        /// <summary>
+        /// A transaction that subtracts funds from the budget.
+        /// </summary>
+        Expense
+    }
+    // highlight-end
+
+    internal class Transaction
+    {
+    }
+}
