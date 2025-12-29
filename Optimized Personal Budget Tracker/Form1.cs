a209b66fbac0cec51ff22fb2e858cc643827003a
GSS1717 <gauravsorathiya33@gmail.com>
2025-12-29 20:01:44 +0530

    Creates Windows Forms Application.

---
+++ Optimized Personal Budget Tracker/Form1.cs
@@ -0,0 +1,10 @@
+namespace Optimized_Personal_Budget_Tracker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
